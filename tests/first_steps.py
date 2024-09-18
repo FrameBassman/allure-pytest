24844d6f93c06642ec3a7ffd638e08f6d4e55af9
Dmitry Romashov <framebassman@gmail.com>
2024-09-18 12:56:04 +0200

    Try to run it

--- tests/first_steps.py
+++ tests/first_steps.py
@@ -1,7 +1,15 @@
+import logging
+import time
+from datetime import timedelta
+
 import allure
 
 
 class FirstSteps():
     @allure.step("Create stuff")
     def create_stuff(self):
+        for index in range(60):
+            logging.getLogger().info("Start sleep for 10 seconds")
+            delay = timedelta(seconds=10)
+            time.sleep(delay.total_seconds())
         pass
